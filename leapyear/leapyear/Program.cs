eba96091a0e0d84174457af083adb17e61263b00
MaheshG <gangurdemahesh9@gmail.com>
2022-10-11 10:27:53 +0530

    [Mahesh]add leapyear program in c#

---
+++ leapyear/leapyear/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+public class leapyear
+{
+    public static void Main(string[] args)
+    {
+        int checkyear;
+        
+
+        Console.WriteLine("Enter the Year");
+
+        checkyear = Convert.ToInt32(Console.ReadLine());
+
+        if ((checkyear % 400) == 0)
+            Console.WriteLine("{0} is a leap year.\n", checkyear);
+        else if ((checkyear % 100) == 0)
+            Console.WriteLine("{0} is not a leap year.\n", checkyear);
+        else if ((checkyear % 4) == 0)
+            Console.WriteLine("{0} is a leap year.\n", checkyear);
+        else
+            Console.WriteLine("{0} is not a leap year.\n", checkyear);
+    }
+}
